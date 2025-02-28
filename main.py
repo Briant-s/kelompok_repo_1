a4f81e611f373abc07977568eca72e112b8feb8d
Briant--S <briantsandriano@gmail.com>
2025-02-28 12:04:47 +0700

    ada bug disini

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+
+class Student:
+    def name(a):
+        return a
+    def NIM(a):
+        return a
+
+
+
 def main():
-    print("Hello from kerkom-proyek-1!")
+    name1 = Student.name("Briant")
+    name2 = Student.name("Alexander")
+    NIM1 = Student.NIM(2802453471)
+    NIM2 = Student.NIM(2802460962)
+    print(name1, NIM1)
+    print(name2, NIM2)    
 
 
 if __name__ == "__main__":
