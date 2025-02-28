7e3c160dd9255c5ea79d3663216909e055326563
xenozite <xeno.zite3@gmail.com>
2025-02-28 14:29:04 +0700

    Instantiate Student objects

--- main.py
+++ main.py
@@ -19,12 +19,8 @@ class Student:
 
 
 def main():
-    name1 = Student.name("Briant")
-    name2 = Student.name("Alexander")
-    NIM1 = Student.NIM(2802453471)
-    NIM2 = Student.NIM(2802460962)
-    print(name1, NIM1)
-    print(name2, NIM2)    
+    student_1 = Student("Briant", "2802453471")
+    student_2 = Student("Alexander", "2802460962")
 
 
 if __name__ == "__main__":
