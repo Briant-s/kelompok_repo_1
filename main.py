1556c7003c3fb99dc9150244e7bafc652c4b9c41
xenozite <xeno.zite3@gmail.com>
2025-03-01 22:00:36 +0700

    Instantiate Alumni object

--- main.py
+++ main.py
@@ -40,18 +40,18 @@ class Alumni(Student):
             Degree: {self.degree}''')
 
 def main():
-    # student_1 = Student()
-    # print('Enter Details for Student 1')
-    # student_1.askDetails()
+    student_1 = Student()
+    print('Enter Details for Student 1')
+    student_1.askDetails()
 
-    # student_2 = Student()
-    # print('Enter Details for Student 2')
-    # student_2.askDetails()
+    student_2 = Student()
+    print('Enter Details for Student 2')
+    student_2.askDetails()
 
-    # print('Student 1 Details : ')
-    # student_1.getDetails()
-    # print('Student 2 Details : ')
-    # student_2.getDetails()
+    print('Student 1 Details : ')
+    student_1.getDetails()
+    print('Student 2 Details : ')
+    student_2.getDetails()
 
     alumni_1 = Alumni()
     print('Enter Details for Alumni 1')
