807dc5742f704a44c262459f8ce86dde9f896642
Briant--S <briantsandriano@gmail.com>
2025-03-01 17:47:42 +0700

    Changed Program Structure

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
 
+def askUser(query):
+    return input(f'Input {query}: ')
+
+
+
 class Student:
 
-    def __init__(self, name, nim):
+    def __init__(self, name = "", gender = "", age = 0, nim = 0):
         self.name = name
+        self.gender = gender
+        self.age = age
         self.nim = nim
-    
-    def getName(self):
-        return self.name
+        
+    def askDetails(self):
+        self.name = askUser('Name')
+        self.gender = askUser('Gender')
+        self.age = askUser('Age')
+        self.nim = askUser('Nim')
+        
 
-    def getNIM(self):
-        return self.nim
-    
-    def name(a):
-        return a
-    def NIM(a):
-        return a
+    def getDetails(self):
+        print(f'''
+            Name: {self.name}
+            Gender: {self.gender}
+            Age : {self.age}
+            NIM : {self.nim}''')
+
 
 
 
 def main():
-    student_1 = Student("Briant", "2802453471")
-    student_2 = Student("Alexander", "2802460962")
+    student_1 = Student()
+    print('Enter Details for Student 1')
+    student_1.askDetails()
+
+    student_2 = Student()
+    print('Enter Details for Student 2')
+    student_2.askDetails()
+
+    print('Student 1 Details : ')
+    student_1.getDetails()
+    print('Student 2 Details : ')
+    student_2.getDetails()
+
+
+    #student_1 = Student("Briant", "2802453471")
+    #student_2 = Student("Alexander", "2802460962")
 
 
 if __name__ == "__main__":
